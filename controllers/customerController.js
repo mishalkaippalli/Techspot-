ff5afcc8fd811795f98910fdcf50eaf79cb382dd
Mishal <mishal9990@gmail.com>
2024-02-26 18:43:46 +0100

    customer controllers added

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -13,3 +13,29 @@ const getCustomersInfo = async (req, res) => {
         console.log(error.message)
     }
 }
+
+const getCustomerBlocked = async (req, res) => {
+    try {
+        let id = req.query.id
+        await User.updateOne({_id: id}, {$set: {isBlocked: true}})
+        res.redirect("/admin/users")
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const getCustomerUnblocked = async (req, res) =>{
+    try {
+        let id = req.query.id
+        console.log(id);
+        await User.updateOne({_id: id}, {$set: {isBlocked: false}})
+        res.redirect("/admin/users")
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+module.exports = { getCustomersInfo,
+                   getCustomerBlocked,
+                   getCustomerUnblocked
+                 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,10 +2,19 @@ const express = require("express");
 const Router = express.Router();
 
 const adminController = require("../controllers/adminController");
+const customerController = require("../controllers/customerController");
 
+//admin actions
 Router.get("/login", adminController.getLoginPage);
 Router.post("/login", adminController.verifyLogin);
 Router.get("/dashboard", adminController.adminDashboard);
+
+//customer managemement
+Router.get("/users", customerController.getCustomersInfo);
+Router.get("/blockCustomer/:id", customerController.getCustomerBlocked);
+Router.get("/unblockCustomer/:id", customerController.getCustomerUnblocked);
+
+
 
 
 module.exports = Router
